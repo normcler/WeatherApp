60dec2fb2c51f69fff32a05dcc898ac6b940b81f
normcler <norm.clerman@gmail.com>
2017-08-12 14:15:43 -0500

    added background image and minor tweaking

--- resources/scripts/weather.js
+++ resources/scripts/weather.js
@@ -8,6 +8,7 @@ var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weath
 The three sections of the API.
 */
 
+// The yahoo api to the weather app.
 var preurl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22";
 var midurl = "%2C%20";
 var posturl = "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
@@ -28,6 +29,12 @@ var getForecast = function (buildURL) {
 
 var postConditions = function(jd) {
     console.log(jd.query.results.channel.item.condition);
+
+    // first post the location
+    var city = jd.query.results.channel.location.city;
+    var region = jd.query.results.channel.location.region;
+    $("#location").text(city + region);
+    
     $("#date").text(jd.query.results.channel.item.condition.date);
     $("#temp").text(jd.query.results.channel.item.condition.temp);
     $("#conditions").text(jd.query.results.channel.item.condition.text);
@@ -60,18 +67,20 @@ var populateCountries = function (data){
 
 function populateStates(data){
     var statesList = $("#select-state");
-    var keys = Object.entries(data);
-    for(var i = 0; i < keys.length; i++){
-        statesList.append("<option value=" + keys[i][0] + " >" + keys[i][1] +
+    //var keys = Object.entries(data);
+    for(var i = 0; i < data.length; i++){
+        statesList.append("<option value=" + data[i].abbreviation + " >" + data[i].name +
                           "</option>");
     }
     console.log(data);
 }
 $.getJSON("https://gist.githubusercontent.com/normcler/05858502b98c56ac9a52913e47262c64/raw/75ab7f5ceb3c1cef5a06ff035aa637fde3bc0852/countries.json", populateCountries);
 
-$.getJSON("https://gist.githubusercontent.com/hello-consumer/747724a59300e3961a993b739ccce9b0/raw/d8677962bf9e2dd157f171391fe2bff7c5192bbf/states.json", populateStates);
+$.getJSON("https://gist.githubusercontent.com/wholypantalones/9916267/raw/e3181b75204863ddd149b7863e9205ed5f09146a/stateprov.json", populateStates);
 
+/*$.getJSON("https://gist.githubusercontent.com/hello-consumer/747724a59300e3961a993b739ccce9b0/raw/d8677962bf9e2dd157f171391fe2bff7c5192bbf/states.json", populateStates);*/
 
+$("#city-field").focus();
 
 /*function(jd) {var wind = jd.query.results.channel.wind; alert(wind.chill);}*/
 
